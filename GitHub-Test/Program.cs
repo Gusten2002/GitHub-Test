4bd88827c3dae797047837d8544ac624e8f4b554
Gusten2002 <August.agren@hotmail.se>
2020-01-30 10:40:10 +0100

    Add project files.

---
+++ GitHub-Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string feels = "no";
+
+            while (feels != "yes")
+            {
+                Console.WriteLine("Are you happy? [Yes/No]");
+                feels = Console.ReadLine().ToLower();
+
+                Console.WriteLine("Yeah, me too :D");
+
+            }
+
+        }
+    }
+}
